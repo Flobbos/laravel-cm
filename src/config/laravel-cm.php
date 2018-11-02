339f7f527f31914d6527a090c163d5899baad5e9
Flobbo <Flobbo@192.168.4.3>
2018-11-02 10:49:36 +0100

    Latest version - incomplete readme file

--- src/config/laravel-cm.php
+++ src/config/laravel-cm.php
@@ -3,11 +3,11 @@
 
 return [
     //Your client API Key from CM
-    'client_api_key' => 'acaeecea00d65c69ae3adfb47ed69f3984c4617feabc19a0',
+    'client_api_key' => '',
     //The client ID from CM
-    'client_id' => 'fcb039bee280ac2a38d376203bf16745',
+    'client_id' => '',
     //If you already have a list
-    'default_list_id' => '19739853070514151a1b374191a4fa18',
+    'default_list_id' => '',
     //Base URI for setting up Guzzle
     'base_uri' => 'https://api.createsend.com/api/v3.2/',
     //Storage path for uploaded imports
