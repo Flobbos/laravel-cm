618c71a604cb701633e09e498e9029f8cb5afebd
FPVZ <freerk.vz@googlemail.com>
2020-01-03 16:16:51 +0100

    Added custom mj-template-style tag to include compiled styles.

--- src/resources/defaults/template/views/template.blade.php
+++ src/resources/defaults/template/views/template.blade.php
@@ -1,7 +1,7 @@
 <mjml>
   <mj-head>
     <mj-title>Welcome to Laravel-CM Base-Template</mj-title>
-    <mj-include path="./style.mjml"/>
+    <mj-template-style/>
   </mj-head>
   <mj-body>
     <mj-wrapper>
