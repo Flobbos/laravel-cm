35d9c8fa55720e508c6289756f2aaa5ba2c62ab6
Alexej Krzewitzki <alexej.k@ultrabold.com>
2021-12-22 16:16:27 +0100

    PHP8 update

--- src/LaravelCM/Contracts/BaseClientContract.php
+++ src/LaravelCM/Contracts/BaseClientContract.php
@@ -2,30 +2,42 @@
 
 namespace Flobbos\LaravelCM\Contracts;
 
-interface BaseClientContract{
-    
+interface BaseClientContract
+{
+
+    /**
+     * Set current options for client
+     *
+     * @param array $options
+     * @return \Flobbos\LaravelCM\BaseClient
+     */
     public function setOptions(array $options): \Flobbos\LaravelCM\BaseClient;
-    
+
+    /**
+     * Get current options for client
+     *
+     * @return array
+     */
     public function getOptions(): array;
-    
+
     /**
      * Set the client API Key
      * @param string $key
      */
     public function setClientApiKey(string $key = null): \Flobbos\LaravelCM\BaseClient;
-    
+
     /**
      * Get the current client API Key
      */
     public function getClientApiKey(): string;
-    
+
     /**
      * Set the client ID
      * @param string $client_id
      * @return \self
      */
     public function setClientID(string $client_id = null): \Flobbos\LaravelCM\BaseClient;
-    
+
     /**
      * Get the current client ID
      * @return string
@@ -37,53 +49,53 @@ interface BaseClientContract{
      * @param string $list_id
      */
     public function setListID(string $list_id = null): \Flobbos\LaravelCM\BaseClient;
-    
+
     /**
      * Get the current List ID
      */
     public function getListID(): string;
-    
+
     /**
      * Set the format of the call
      * @param string $format json|xml
      */
     public function setFormat(string $format): \Flobbos\LaravelCM\BaseClient;
-    
+
     /**
      * Get the current format in use
      */
     public function getFormat(): string;
-    
+
     /**
      * Get a clean guzzle client object
      */
     public function getGuzzle(): \GuzzleHttp\Client;
-    
+
     /**
      * Initialize guzzle with a base_uri called by the constructor
      * @param type $base_uri
      */
     public function initGuzzle($base_uri = null): \Flobbos\LaravelCM\BaseClient;
-    
+
     /**
      * Retrieve basic Guzzle options for making an API call
      * @return array
      */
     public function getAuthInformation(): array;
-    
+
     /**
      * Retrieve basic Guzzle options for making an API call
      * @return array
      */
     public function getBaseRequestData(): array;
-    
+
     /**
      * Merge the request data with parameters provided by calling function
      * @param array $request_data
      * @return type
      */
     public function mergeRequestData(array $request_data): array;
-    
+
     /**
      * Call the guzzle Client and provide optional validation key to skip
      * when validating options
@@ -91,13 +103,12 @@ interface BaseClientContract{
      * @return Guzzle\Client
      */
     public function callApi(): \GuzzleHttp\Client;
-    
+
     /**
      * Make API call to retrieve data
      * @param type $method
      * @param type $url
      * @param array $request_data
      */
-    public function makeCall($method = 'get', $url, array $request_data);
-    
+    public function makeCall($url, array $request_data, $method = 'get');
 }
