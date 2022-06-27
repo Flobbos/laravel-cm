5951c54f79b7b40d381270cd4a21ce826b1c00a1
Flobbo <alexej@helloo.it>
2022-06-27 18:27:52 +0200

    Removed edit from campaigns Updated Bootstrap4 views

--- src/resources/views/bootstrap4/campaigns/schedule.blade.php
+++ src/resources/views/bootstrap4/campaigns/schedule.blade.php
@@ -1,58 +1,63 @@
-@extends('layouts.'.config('laravel-cm.layout_file'))
+@extends('layouts.' . config('laravel-cm.layout_file'))
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12">
-            <div class="card">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
 
-                <form action="{{ route('laravel-cm::campaigns.send',$campaign->CampaignID) }}" role="form" method="POST"  enctype="multipart/form-data">
-                    {{ csrf_field() }}
+                    <form action="{{ route('laravel-cm::campaigns.send', $campaign->CampaignID) }}" role="form"
+                        method="POST" enctype="multipart/form-data">
+                        {{ csrf_field() }}
 
-                    <div class="card-header">
-                        <h3>{{$campaign->Name}}</h3>
-                        @lang('laravel-cm::campaigns.schedule_title')
-                    </div>
-
-                    <div class="card-body">
-
-                        @include('laravel-cm::notifications')
-                        
-                        @lang('laravel-cm::campaigns.preview'): <a href="{{$campaign->PreviewURL}}" target="_blank"><strong>{{$campaign->PreviewURL}}</strong></a><br />
-                        
-                        <div class="form-group">
-                            <label class="control-label" for="ConfirmationEmail">@lang('laravel-cm::campaigns.confirmation_emails_title')</label>
-                            <input class="form-control" placeholder="@lang('laravel-cm::campaigns.confirmation_emails_placeholder')" type="text" name="ConfirmationEmail" value="{{ old('ConfirmationEmail') }}" />
+                        <div class="card-header">
+                            <h3>{{ $campaign->Name }}</h3>
+                            @lang('laravel-cm::campaigns.schedule_title')
                         </div>
 
-                        <div class="form-group">
-                            <label class="control-label" for="SendDate">@lang('laravel-cm::campaigns.send_date')</label>
-                            <vue-date-picker name="SendDate"></vue-date-picker>
-                        </div>
+                        <div class="card-body">
 
-                    </div>
+                            @include('laravel-cm::notifications')
 
-                    <div class="card-footer">
+                            @lang('laravel-cm::campaigns.preview'): <a href="{{ $campaign->PreviewURL }}"
+                                target="_blank"><strong>{{ $campaign->PreviewURL }}</strong></a><br />
 
-                        <div class="row">
-
-                            <div class="col-sm-6">
-                                <a href="{{ route('laravel-cm::campaigns.index') }}" class="btn btn-danger">@lang('laravel-cm::crud.cancel')</a>
+                            <div class="form-group">
+                                <label class="control-label" for="ConfirmationEmail">@lang('laravel-cm::campaigns.confirmation_emails_title')</label>
+                                <input class="form-control" placeholder="@lang('laravel-cm::campaigns.confirmation_emails_placeholder')" type="text"
+                                    name="ConfirmationEmail" value="{{ old('ConfirmationEmail') }}" />
                             </div>
 
-                            <div class="col-sm-6 text-right">
-                                <button type="submit" class="btn btn-success">@lang('laravel-cm::crud.save')</button>
+                            <div class="form-group">
+                                <label class="control-label" for="SendDate">@lang('laravel-cm::campaigns.send_date')</label>
+                                <input type="text" class="form-control" name="SendDate"
+                                    placeholder="Format: yyyy-mm-dd HH:mm" />
                             </div>
 
                         </div>
 
-                    </div>
+                        <div class="card-footer">
 
-                </form>
+                            <div class="row">
 
+                                <div class="col-sm-6">
+                                    <a href="{{ route('laravel-cm::campaigns.index') }}"
+                                        class="btn btn-danger">@lang('laravel-cm::crud.cancel')</a>
+                                </div>
+
+                                <div class="col-sm-6 text-right">
+                                    <button type="submit" class="btn btn-success">@lang('laravel-cm::crud.save')</button>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </form>
+
+                </div>
             </div>
-        </div>
 
+        </div>
     </div>
-</div>
 @stop
