dcbc20a756a6db176820698ec0504da6cac9907b
Alexej Krzewitzki <alexej.k@ultrabold.com>
2023-04-04 15:37:09 +0200

    Fixed mutators

--- src/LaravelCM/Models/NewsletterTemplate.php
+++ src/LaravelCM/Models/NewsletterTemplate.php
@@ -18,28 +18,28 @@ class NewsletterTemplate extends Model
     public function templateUrl(): Attribute
     {
         return Attribute::make(
-            get: fn (string $value) => url('storage/' . config('laravel-cm.asset_path') . '/' . $this->template_name)
+            get: fn () => url('storage/' . config('laravel-cm.asset_path') . '/' . $this->template_name)
         );
     }
 
     public function templateFileUrl(): Attribute
     {
         return Attribute::make(
-            get: fn (string $value) => $this->template_url . '/' . $this->template_name . '.html'
+            get: fn () => $this->template_url . '/' . $this->template_name . '.html'
         );
     }
 
     public function templatePath(): Attribute
     {
         return Attribute::make(
-            get: fn (string $value) => storage_path('app/public/' . config('laravel-cm.asset_path') . '/' . $this->template_name)
+            get: fn () => storage_path('app/public/' . config('laravel-cm.asset_path') . '/' . $this->template_name)
         );
     }
 
     public function templateFilePath()
     {
         return Attribute::make(
-            get: fn (string $value) => $this->template_path . '/' . $this->template_name . '.html'
+            get: fn () => $this->template_path . '/' . $this->template_name . '.html'
         );
     }
 }
